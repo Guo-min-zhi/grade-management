a9b09a5657ed50c955aec187b0d963f65e84fcae
minzhi <eric.minzhi.guo@gmail.com>
2014-05-28 10:09:26 +0800

    java init

---
+++ code/egms/src/main/java/org/bjtuse/egms/App.java
@@ -0,0 +1,13 @@
+package org.bjtuse.egms;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
