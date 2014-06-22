384ea6f1254e195ac60f1c3143b1e6a8a4fad573
believe1499 <believe1499@gmail.com>
2014-06-22 23:08:58 +0800

    数据库变更

--- docs/update.sql
+++ docs/update.sql
@@ -1,2 +1,9 @@
 ALTER TABLE `certificate_type`
 ADD COLUMN `upload_type`  tinyint NULL AFTER `formula`;
+
+ALTER TABLE `certificate_score`
+ADD COLUMN `oral_score`  float NULL AFTER `grade_final`,
+ADD COLUMN `written_score`  float NULL AFTER `oral_score`;
+
+ALTER TABLE `certificate_score`
+ADD COLUMN `grade_status`  tinyint NULL AFTER `written_score`;
